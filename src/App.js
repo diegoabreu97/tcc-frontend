46bf2327a8ab782341f0e5df8399ce9c81e5487c
unknown <41683@fiecalunos.lan>
2025-08-28 22:39:43 -0300

    modificao 2808

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import './index.css'; // Certifique-se de que o CSS global está importado
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import Register from './Register';
+import ForgotPassword from './ForgotPassword'; // Se você tiver criado este componente
+import HomeScreen from './HomeScreen'; // Importe a tela inicial
 
-function App() {
+const App = () => {
   return (
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/home" element={<HomeScreen />} /> {/* Nova rota para a tela inicial */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
